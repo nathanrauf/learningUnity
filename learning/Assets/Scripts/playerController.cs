890333dc12082b9762052c75531098f746e1da33
nathanrauf <nathanrauf@gmail.com>
2018-10-02 14:49:39 -0400

    all

---
+++ learning/Assets/Scripts/playerController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerController : MonoBehaviour {
+
+
+    Rigidbody2D rb;
+    public float movementSpeed = 5f;
+    public float jumpHeight = 5f;
+    bool onGround;
+    public Transform groundCheck;
+    public LayerMask ground;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+	// Update is called once per frame
+	void FixedUpdate () {
+
+        //horizontal movement
+        float move = Input.GetAxis("Horizontal");
+        rb.velocity = new Vector2(move * movementSpeed, rb.velocity.y);
+
+        //onGround
+        onGround = Physics2D.OverlapCircle(groundCheck.position, .2f, ground);
+        
+	}
+
+    void Update()
+    {
+        //jumpman
+        if(onGround && Input.GetButtonDown("Jump")){
+            rb.AddForce(new Vector2(0,jumpHeight));
+        }
+         
+    }
+}
---
+++ learning/Assets/playerMovement.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMovement : MonoBehaviour {
+
+    Rigidbody2D rb;
+
+
+    void Start () {
+
+    }
+
+
+	// Update is called once per frame
+	void FixedUpdate () {
+		
+	}
+}
